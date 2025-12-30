3e85b4a16d3e78e21bec82f167d05d60e00b30f7
Dinis Cruz <dinis.cruz@owasp.org>
2025-12-30 03:11:08 +0000

    wired in Model__MGraph__Node__Factory refactored MGraph__Type__Resolver(Type_Safe): to improve performance added multiple @timestamp decorators

--- mgraph_db/mgraph/models/Model__MGraph__Graph.py
+++ mgraph_db/mgraph/models/Model__MGraph__Graph.py
@@ -1,38 +1,50 @@
-from typing                                                             import List
-from mgraph_db.mgraph.schemas.Schema__MGraph__Node__Data                import Schema__MGraph__Node__Data
-from osbot_utils.helpers.timestamp_capture.decorators.timestamp         import timestamp
-from osbot_utils.type_safe.primitives.domains.identifiers.Obj_Id        import Obj_Id
-from osbot_utils.type_safe.primitives.domains.identifiers.Edge_Id       import Edge_Id
-from osbot_utils.type_safe.primitives.domains.identifiers.Node_Id       import Node_Id
-from osbot_utils.type_safe.primitives.domains.identifiers.Graph_Id      import Graph_Id
-from osbot_utils.type_safe.type_safe_core.decorators.type_safe          import type_safe
-from osbot_utils.type_safe.type_safe_core.methods.type_safe_property    import set_as_property
-from osbot_utils.type_safe.type_safe_core.shared.Type_Safe__Cache       import type_safe_cache
-from mgraph_db.mgraph.schemas.Schema__MGraph__Node__Value__Data         import Schema__MGraph__Node__Value__Data
-from mgraph_db.mgraph.models.Model__MGraph__Types                       import Model__MGraph__Types
-from mgraph_db.mgraph.models.Model__MGraph__Edge                        import Model__MGraph__Edge
-from mgraph_db.mgraph.models.Model__MGraph__Node                        import Model__MGraph__Node
-from mgraph_db.mgraph.schemas.Schema__MGraph__Graph                     import Schema__MGraph__Graph
-from mgraph_db.mgraph.schemas.Schema__MGraph__Node                      import Schema__MGraph__Node
-from mgraph_db.mgraph.schemas.Schema__MGraph__Edge                      import Schema__MGraph__Edge
-from mgraph_db.mgraph.actions.MGraph__Type__Resolver                    import MGraph__Type__Resolver
-from osbot_utils.type_safe.Type_Safe                                    import Type_Safe
+from typing                                                                 import List
+from osbot_utils.decorators.methods.cache_on_self                           import cache_on_self
+from osbot_utils.helpers.timestamp_capture.context_managers.timestamp_block import timestamp_block
+from osbot_utils.helpers.timestamp_capture.decorators.timestamp             import timestamp
+from osbot_utils.type_safe.primitives.domains.identifiers.Edge_Id           import Edge_Id
+from osbot_utils.type_safe.primitives.domains.identifiers.Node_Id           import Node_Id
+from osbot_utils.type_safe.primitives.domains.identifiers.Graph_Id          import Graph_Id
+from osbot_utils.type_safe.type_safe_core.decorators.type_safe              import type_safe
+from osbot_utils.type_safe.type_safe_core.methods.type_safe_property        import set_as_property
+from mgraph_db.mgraph.models.Model__MGraph__Types                           import Model__MGraph__Types
+from mgraph_db.mgraph.models.Model__MGraph__Edge                            import Model__MGraph__Edge
+from mgraph_db.mgraph.models.Model__MGraph__Node                            import Model__MGraph__Node
+from mgraph_db.mgraph.schemas.Schema__MGraph__Graph                         import Schema__MGraph__Graph
+from mgraph_db.mgraph.schemas.Schema__MGraph__Node                          import Schema__MGraph__Node
+from mgraph_db.mgraph.schemas.Schema__MGraph__Edge                          import Schema__MGraph__Edge
+from mgraph_db.mgraph.actions.MGraph__Type__Resolver                        import MGraph__Type__Resolver
+from osbot_utils.type_safe.Type_Safe                                        import Type_Safe
 
 
 
 class Model__MGraph__Graph(Type_Safe):
-    data       : Schema__MGraph__Graph
-    model_types: Model__MGraph__Types
-    resolver   : MGraph__Type__Resolver                                                     # Auto-instantiated - provides type resolution
+    data        : Schema__MGraph__Graph
+    model_types : Model__MGraph__Types
+    resolver    : MGraph__Type__Resolver                                                     # Auto-instantiated - provides type resolution
 
     graph_id = set_as_property('data', 'graph_id', Graph_Id)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.node_factory()        # trigger object creation (so that we don't have a little spike on the @timestamp stats)
+
+    @cache_on_self
+    @timestamp(name='get__node_factory()')
+    def node_factory(self):
+        from mgraph_db.mgraph.models.Model__MGraph__Node__Factory import Model__MGraph__Node__Factory       # todo: find a better way to do this so that we don't have this circular dependency
+        return Model__MGraph__Node__Factory(graph=self)
+
+
     @type_safe
+    @timestamp(name="model.mgraph.add_node")
     def add_node(self, node: Schema__MGraph__Node) -> Model__MGraph__Node:                  # Add a node to the graph
         self.data.nodes[node.node_id] = node
         type_value                    = self.model_types.node_model_type if self.model_types else None
-        node_model_type               = self.resolver.node_model_type(type_value)
-        return node_model_type(data=node)
+        with timestamp_block(name="model.mgraph.add_node.node_model_type (find)"):
+            node_model_type               = self.resolver.node_model_type(type_value)
+        with timestamp_block(name="model.mgraph.add_node.node_model_type (invoke)"):
+            return node_model_type(data=node)
 
     @type_safe
     def add_edge(self, edge: Schema__MGraph__Edge) -> Model__MGraph__Edge:                  # Add an edge to the graph
@@ -62,62 +74,9 @@ class Model__MGraph__Graph(Type_Safe):
         edge = kwargs.get('edge_type')(**kwargs)
         return self.add_edge(edge)
 
-    @timestamp(name='new_node (model_mgraph) - WE ARE FOCUSING HERE :) ')
-    def new_node(self, **kwargs):
-        if 'node_type' in kwargs and 'node_data' in kwargs:                                 # if node_type and node_data is provided, then we have all we need to create the new node
-            node_type = kwargs.get('node_type')
-            node_data = kwargs.get('node_data')
-            #del kwargs['node_type']
-            del kwargs['node_data']
-            node      = node_type(node_data=node_data, **kwargs)
-            return self.add_node(node)
-
-        add_node_type_to_node = False
-        if 'node_type' in kwargs:
-            node_type              = kwargs.get('node_type')                                # if we have the node_type here
-            node_type__annotations = dict(type_safe_cache.get_class_annotations(node_type)) # get its annotations
-            node_data_type         = node_type__annotations.get('node_data')                # so that we can resolve the node_data object
-        elif self.data.schema_types is not None:                                            # Check for schema_types
-            node_type      = self.data.schema_types.node_type
-            node_data_type = self.data.schema_types.node_data_type
-            node_type      = self.resolver.node_type(node_type)                             # Resolve if None
-            node_data_type = self.resolver.node_data_type(node_data_type)                   # Resolve if None
-            node_type__annotations = dict(type_safe_cache.get_class_annotations(node_type))
-            add_node_type_to_node = True
-        elif self.model_types.node_model_type:
-            node_type = self.model_types.node_model_type.__annotations__.get('data')
-            node_type__annotations = dict(type_safe_cache.get_class_annotations(node_type)) # get its annotations
-            node_data_type         = node_type__annotations.get('node_data')                # so that we can resolve the node_data object
-            add_node_type_to_node = True
-        else:                                                                               # Fallback to defaults
-            node_type      = self.resolver.node_type(None)
-            node_data_type = self.resolver.node_data_type(None)
-            node_type__annotations = dict(type_safe_cache.get_class_annotations(node_type))
-
-
-        node_type__kwargs           = {}                                                    # Separate kwargs for node_type and node_data_type
-        node_data__type_kwargs      = {}
-        node_data_type__annotations = dict(type_safe_cache.get_class_annotations(node_data_type))
-
-        for key, value in kwargs.items():                                                   # todo: review this 'feature' to split the kwargs based on the node and the data class
-            if key in node_type__annotations:                                               #       there could be some cases where this is useful (like how it is used in the mermaid provider
-                node_type__kwargs[key] = value                                              #       but in general this is not a good pattern to follow
-            if key in node_data_type__annotations:
-                node_data__type_kwargs[key] = value
-        if node_data_type is Schema__MGraph__Node__Data:                                    # if this is a Schema__MGraph__Node__Data
-            node_data = None                                                                # then we don't need to create the object since Schema__MGraph__Node__Data has not attributes
-        else:
-            if issubclass(node_data_type, Schema__MGraph__Node__Value__Data):                   # handle edge case which happens when we are creating a new value node
-                if node_data__type_kwargs == {}:                                                # but have not provided any value
-                    node_data__type_kwargs['key'] = Node_Id(Node_Id(Obj_Id()))                  # which means we need to make sure this is an unique node (or it can't be indexed)
-            node_data = node_data_type(**node_data__type_kwargs                )                # Create node data object           # todo: see if this is be test way (and location) to handle this
-
-        node      = node_type     (node_data=node_data, **node_type__kwargs)                # Create a node with the node data
-
-        if add_node_type_to_node:                                                          # if this is set, it means that we are not using the defaults (or None value)
-            node.node_type = node_type                                                     #   which means that we need to set this value
-
-        return self.add_node(node)
+    @timestamp(name='new_node (model_mgraph) - REFACTORED ')
+    def new_node(self, **kwargs):                                                               # Delegate to factory
+        return self.node_factory().create_node(**kwargs)
 
     def edges(self):
         edge_model_type = self.resolver.edge_model_type(
